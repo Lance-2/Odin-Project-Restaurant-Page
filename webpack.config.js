b992aa9f6878a5f2c3653c1315aedf89f74c0e27
Lance-2 <lancekristanmartin@gmail.com>
2026-09-24 10:26:58 +0800

    CSS Configuration

--- webpack.config.js
+++ webpack.config.js
@@ -18,4 +18,12 @@ module.exports = {
     static: './dist',
     open: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
